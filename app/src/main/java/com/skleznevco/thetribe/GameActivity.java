95f30ea788198e0d3b403d8a4ee62bee06b86821
Skleznev <job.skleznev@gmail.com>
2020-03-11 23:31:59 +0500

    Initial commit

---
+++ app/src/main/java/com/skleznevco/thetribe/GameActivity.java
@@ -0,0 +1,16 @@
+package com.skleznevco.thetribe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/com/skleznevco/thetribe/Resource.java
@@ -0,0 +1,19 @@
+package com.skleznevco.thetribe;
+
+public class Resource {
+    Item food;
+    Item stone;
+    Item wood;
+    Item gold;
+
+    
+
+
+
+}
+
+class Item{
+    int profit;
+    int lose;
+    int total;
+}
