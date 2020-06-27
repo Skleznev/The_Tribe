5a26e21234d745f177eda65f54812a022fa4a45d
Zotovaps <zotovaps2000@gmail.com>
2020-06-27 20:03:34 +0500

    Добавила варианты хороших/плохих событий

--- app/src/main/java/com/skleznevco/thetribe/ChanceDialog.java
+++ app/src/main/java/com/skleznevco/thetribe/ChanceDialog.java
@@ -18,10 +18,48 @@ public class ChanceDialog extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        if (random.nextInt(100) + 1 < GameRules.getLuckyChance()*100) {
-            eventMessage = "Хорошее событие";
+        if (random.nextInt(100) + 1 < GameRules.getLuckyChance() * 100) {
+            switch (random.nextInt(6)) {
+                case 0:
+                    eventMessage = "Поселенцы вашего племени нашли клад";
+                    break;
+                case 1:
+                    eventMessage = "К вашему племени присоединились мимо идущие путешественники";
+                    break;
+                case 2:
+                    eventMessage = "К вашему племени присоединились сторонние военные";
+                    break;
+                case 3:
+                    eventMessage = "Период дождей - урожай увеличился";
+                    break;
+                case 4:
+                    eventMessage = "Поселенцы вашего племени нашли заброшенную каменоломню";
+                    break;
+                case 5:
+                    eventMessage = "Поселенцы вашего племени нашли заброшенную лесопильню";
+                    break;
+            }
         } else {
-            eventMessage = "Плохое событие";
+            switch (random.nextInt(6)) {
+                case 0:
+                    eventMessage = "Период засухи - урожай погиб";
+                    break;
+                case 1:
+                    eventMessage = "На вашем складе завелись крысы";
+                    break;
+                case 2:
+                    eventMessage = "На вашей каменоломне камень упал на рабочих";
+                    break;
+                case 3:
+                    eventMessage = "На вашей лесопильне произашел пожар";
+                    break;
+                case 4:
+                    eventMessage = "Военные вашего племени дизертировали";
+                    break;
+                case 5:
+                    eventMessage = "Воры украли казну";
+                    break;
+            }
         }
 
         return builder.setTitle("Диалоговое окно").setMessage(eventMessage).create();
